8949f67d67377eec4863b2b574ab67214d64be5e
Alexandr Guselnikov <guselnikov1988@ngs.ru>
2019-03-18 17:08:52 +0700

    add display map

--- src/app/components/tickets-form/tickets-form.component.ts
+++ src/app/components/tickets-form/tickets-form.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { debounceTime, switchMap } from 'rxjs/operators';
 import * as moment from 'moment-timezone';
 import { Store } from '@ngrx/store';
+import ymaps from 'ymaps';
 
 import { CitiesService } from '../../services/cities.service';
 import { ICity } from '../../models/city.interface';
@@ -22,6 +23,11 @@ export class TicketsFormComponent implements OnInit {
   filteredCitiesFrom$: Observable<ICity[]>;
   filteredCitiesTo$: Observable<ICity[]>;
   private counter = 0;
+  private ymaps;
+  private mapCities;
+  private API_KEY = 'b0f826bc-64e4-4213-a834-a4e6da9173d4';
+  private DEFAULT_CENTER = [55.000665013597725, 82.95603900000002];
+  private DEFAULT_ZOOM = 6;
 
   constructor(private fb: FormBuilder, private citiesService: CitiesService, private store: Store<IAppState>) { }
 
@@ -39,6 +45,7 @@ export class TicketsFormComponent implements OnInit {
       .pipe(
         debounceTime(300),
         switchMap(value => {
+          this.updateMapCities();
           if (typeof value === 'string') {
             return this.citiesService.search(value);
           }
@@ -52,12 +59,15 @@ export class TicketsFormComponent implements OnInit {
       .pipe(
         debounceTime(300),
         switchMap(value => {
+          this.updateMapCities();
           if (typeof value === 'string') {
             return this.citiesService.search(value);
           }
           return [];
         })
       );
+
+    this.initMapCities();
   }
 
   displayCity(city: ICity) {
@@ -90,5 +100,58 @@ export class TicketsFormComponent implements OnInit {
       timeTo
     };
     this.store.dispatch(new AddTicket(ticket));
+  }
+
+  private initMapCities() {
+    ymaps.load(`https://api-maps.yandex.ru/2.1/?apikey=${this.API_KEY}&lang=ru_RU`)
+      .then(maps => {
+        this.ymaps = maps;
+        this.mapCities = new this.ymaps.Map('map-cities', {
+          center: this.DEFAULT_CENTER,
+          zoom: this.DEFAULT_ZOOM,
+          controls: ['zoomControl']
+        });
+      });
+  }
+
+  private updateMapCities() {
+    const {cityFrom, cityTo} = this.ticketForm.value;
+    const promiseList = [];
+    if (cityFrom && cityFrom.name) {
+      promiseList.push(this.ymaps.geocode(cityFrom.name));
+    }
+    if (cityTo && cityTo.name) {
+      promiseList.push(this.ymaps.geocode(cityTo.name));
+    }
+
+    if (promiseList.length) {
+      Promise.all(promiseList)
+        .then(resultList => {
+          const myCollection = new this.ymaps.GeoObjectCollection();
+          resultList.forEach(res => {
+            myCollection.add(res.geoObjects.get(0));
+          });
+
+          this.mapCities.geoObjects.removeAll();
+          this.mapCities.geoObjects.add(myCollection);
+
+          if (myCollection.getLength() === 1) {
+            this.mapCities.setCenter(myCollection.get(0).geometry._coordinates, this.DEFAULT_ZOOM);
+          } else {
+            const centerAndZoom = this.ymaps.util.bounds.getCenterAndZoom(
+              myCollection.getBounds(),
+              this.mapCities.container.getSize(),
+              this.mapCities.options.get('projection')
+            );
+            this.mapCities.setCenter(
+              [centerAndZoom.center[0], centerAndZoom.center[1]],
+              centerAndZoom.zoom
+            );
+          }
+        });
+    } else {
+      this.mapCities.geoObjects.removeAll();
+      this.mapCities.setCenter(this.DEFAULT_CENTER, this.DEFAULT_ZOOM);
+    }
   }
 }
